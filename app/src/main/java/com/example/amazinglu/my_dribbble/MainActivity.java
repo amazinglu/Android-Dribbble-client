9af358114f8de9227b314b372ed28429655576a0
AmazingLu <amazinglu166@gmail.com>
2017-11-10 15:46:28 -0500

    finish the function of nagivation drawer

--- app/src/main/java/com/example/amazinglu/my_dribbble/MainActivity.java
+++ app/src/main/java/com/example/amazinglu/my_dribbble/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.amazinglu.my_dribbble;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.amazinglu.my_dribbble.shot_list.ShotListFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer) NavigationView navigationView;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // enable the back button on the action bar
+        // which is for sandwich button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        setupDrawer();
 
         /**
          * savedInstanceState:
@@ -23,5 +48,97 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, ShotListFragment.newInstance())
                     .commit();
         }
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        /**
+         * tell the drawerToggle the current state
+         * state 1: drawer is open
+         * state 2: drawer is closed
+         * use for the icon and the animation of the sandwich button
+         * */
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        /**
+         * when the drawer close and open, the configuration is change
+         * this function help the OS to setup new configuration
+         * */
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /**
+         * the click event of the sandwich button
+         * */
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawer() {
+        /**
+         * the listener of drawer which is used to change the state of the action bar
+         * */
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        /**
+         * the listener of the items in navigation view
+         * */
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // if the item has been choose before
+                // no new fragment is needed
+                if (item.isChecked()) {
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.drawer_item_home:
+                        fragment = ShotListFragment.newInstance();
+                        setTitle(R.string.drawer_menus_home_title);
+                        break;
+                    case R.id.drawer_item_likes:
+                        fragment = ShotListFragment.newInstance();
+                        setTitle(R.string.drawer_menus_like_title);
+                        break;
+                    case R.id.drawer_item_buckets:
+                        fragment = ShotListFragment.newInstance();
+                        setTitle(R.string.drawer_menus_bucket_title);
+                        break;
+                }
+
+                /**
+                 * close the drawer after the click element
+                 * */
+                drawerLayout.closeDrawers();
+
+                /**
+                 * replace the fragment if needed
+                 * */
+                if (fragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
