a5a354cb6d12e48a4ffedcbaaabd9caf62c4af4a
AmazingLu <amazinglu166@gmail.com>
2017-11-25 23:41:56 -0500

    keyboard not blocking edit text

--- app/src/main/java/com/example/amazinglu/my_dribbble/base/SpaceItemdecoration.java
+++ app/src/main/java/com/example/amazinglu/my_dribbble/base/SpaceItemdecoration.java
@@ -17,12 +17,12 @@ public class SpaceItemdecoration extends RecyclerView.ItemDecoration {
 
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        outRect.left = space;
-        outRect.right = space;
+//        outRect.left = space;
+//        outRect.right = space;
         outRect.bottom = space;
 
-        if (parent.getChildAdapterPosition(view) == 0) {
-            outRect.top = space;
-        }
+//        if (parent.getChildAdapterPosition(view) == 0) {
+//            outRect.top = space;
+//        }
     }
 }
